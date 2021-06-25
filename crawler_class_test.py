b9e40fc9001a77a677a321cf08deee89dbe66871
honginyeong <hongreat95@gmail.com>
2021-06-25 10:20:46 +0900

    폴더 및 파일 정리

--- crawler_class_test.py
+++
@@ -1,8 +0,0 @@
-from crawler_class import CafeCrawler
-
-crawler = CafeCrawler(file='html_source.txt')
-print(crawler.generate_dataframe())
-
-print(crawler.filter_view(30, high = 40))
-
-print(crawler.filter_date('2021.06.14', high='2021.06.17'))
